6329b6ea105780a1dbc4cd027c3431f725381aad
lhao <hldset111@aq.com>
2017-12-18 23:26:52 +0800

    1

--- framework.py
+++ framework.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python
+# 20171218     init    Liang Hao
 import MySQLdb
+from os import listdir
+from os.path import isfile, join
 
 db = MySQLdb.connect(host="139.224.10.176",    # your host, usually localhost
                      user="hadoop",         # your username
                      passwd="mysql",  # your password
                      db="KB")        # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
 cur = db.cursor()
 
-# Use all the SQL you like
-cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+# Get landing directory
+cur.execute("SELECT value FROM parameter where name = 'LandingDir'")
 
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print row[0]
+landingDir = cur.fetchone()
+
+# Get regular files under landing directory
+fileList = [f for f in listdir(landingDir) if isfile(join(landingDir, f))]
+
+# For each file get customer ID
+fileCustomer = { }
+for f in fileList:
+    cur.execute("SELECT id FROM (SELECT * FROM customer, (SELECT '" + f + "' as current_file) a WHERE current_file like 'file_name'")
+    if id:
+        fileCustomer[f] = {'customer_id': id}
+
+# Get rules
+for f in fileCustomer:
+    cur.execute("SELECT rule_id, step_id FROM customer_rule WHERE customer_id = " + fileCustomer[f]['customer_id'])
+    fileCustomer[f]['customer_id'] = cur.fetchall()
+
 
 db.close()
